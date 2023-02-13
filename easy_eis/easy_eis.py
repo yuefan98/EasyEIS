27e01bef6bf25a4282193c284316e892be09495e
Yuefan Ji <yuefanji@Yuefans-iMac-2.local>
2023-02-12 18:07:14 -0800

    initial prototype

---
+++ easy_eis/easy_eis.py
@@ -0,0 +1,136 @@
+
+import numpy as np 
+import pandas as pd
+import scipy
+import matplotlib.pyplot as plt
+from io import StringIO, BytesIO
+
+class elements:
+    def __init__(self,ax,x,y,lw):
+        self.x = x
+        self.y = y
+        self.lw = lw
+        self.ax = ax
+
+    def R(self):
+        x0 = np.linspace(0,7,15)
+        y0 = np.array([0,0,0.3,0,-0.3,0,0.3,0,-0.3,0,0.3,0,-0.3,0,0])
+        self.ax.plot(self.x+x0, self.y+y0, color='black',lw=self.lw)
+
+        self.x += x0[-1]
+        self.y += y0[-1]
+    def W(self):
+        x0 = np.linspace(1,6,9)
+        y0 = np.array([0.3,0,-0.3,0,0.3,0,-0.3,0,0.3])
+        self.ax.plot(self.x+x0, self.y+y0, color='black',lw=self.lw)
+        self.ax.plot(self.x+np.array([0,1.625]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.ax.plot(self.x+np.array([5.375,7]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.x += 7
+        self.y += 0
+
+    def C(self):
+        lm = 2
+        rm = 5
+        self.ax.plot(self.x+np.array([lm,lm]), self.y+np.array([-0.3,0.3]), color='black',lw=self.lw)
+        self.ax.plot(self.x+np.array([rm,rm]), self.y+np.array([-0.3,0.3]), color='black',lw=self.lw)
+        self.ax.plot(self.x+np.array([0,lm]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.ax.plot(self.x+np.array([rm,7]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.x += 7
+        self.y += 0
+    def line(self):
+        length = 7
+        self.ax.plot(self.x+np.array([0,length]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.x += length
+        self.y += 0
+    def long_line(self):
+        length = 14
+        self.ax.plot(self.x+np.array([0,length]), self.y+np.array([0,0]), color='black',lw=self.lw)
+        self.x += length
+        self.y += 0
+        
+    def p(self,upper_elm,lower_elm):
+        height = 0.5
+        x_p = self.x
+        y_p = self.y 
+        self.ax.plot(self.x+np.array([0,0]), self.y+np.array([-height,height]), color='black',lw=self.lw)
+        self.x += 0 
+        self.y += height
+        up_len = len(upper_elm)
+        low_len = len(lower_elm)
+        if up_len >= low_len:
+            self.line()
+        else:
+            for i in range (0,low_len-up_len+1):
+                self.line()
+        for i in range (0,up_len):
+                    upper_elm[i]()
+                    self.line()
+        if up_len < low_len:
+            for i in range (0,low_len-up_len):
+                self.line()
+        self.x = x_p
+        self.y = y_p-height
+        if up_len <= low_len:
+            self.line()
+        else:
+            for i in range (0,up_len-low_len+1):
+                self.line()
+        for i in range(0,low_len):
+            lower_elm[i]()
+            self.line()
+        if low_len < up_len:
+            for i in range (0,up_len-low_len):
+                self.line()
+
+        self.y = y_p
+        self.ax.plot(self.x+np.array([0,0]), self.y+np.array([-height,height]), color='black',lw=self.lw)
+    def save(self,name,dpi,w,h,transparent=True):
+
+        fig =self.get_fig(w, h)
+        fig.savefig(name, bbox_inches='tight', transparent=transparent, dpi=dpi)
+        
+    def get_fig(self,w,h):
+        self.ax.axes.get_xaxis().set_visible(False)
+        self.ax.axes.get_yaxis().set_visible(False)
+        self.ax.set_frame_on(False)
+        fig = self.ax.get_figure().set_size_inches(w,h)
+        return(fig)
+        
+    def getimage(self, w,h,fmt='png'):
+
+        fig = self.get_fig(w,h)
+        output = BytesIO()
+        plt.savefig(output, format=fmt, bbox_inches='tight')
+        image = output.getvalue()
+        return(image)
+
+def impedance_data_processing(text_file,option):
+    '''
+    
+    Parameters
+    ----------
+    text_file : str
+        file name of the impedance data.
+    Returns
+    -------
+    the dataframe with column 'Z1', 'Z2', and 'frequency'.
+    '''
+    data=np.loadtxt(text_file,delimiter=",",skiprows=11)
+    f=data[:,0]
+    Z1=data[:,4]
+    Z2=data[:,5]
+    if option == 'No':
+        
+        mask = np.array(Z2)<0
+        f = f[mask]
+        Z1 = Z1[mask]
+        Z2 = Z2[mask]
+    Z=np.array(Z1) +1j*np.array(Z2)
+    return(f,Z)
+
+    
+    
+    
+    
+    
+    
---
+++ web_interface.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 13:10:00 2023
+
+@author: yuefanji
+"""
+
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import scipy
+import matplotlib.pyplot as plt
+from impedance.models.circuits import CustomCircuit
+from impedance.validation import linKK
+import schemdraw
+import schemdraw.elements as elm
+from  easy_eis import *
+from io import StringIO, BytesIO
+from impedance.visualization import plot_nyquist
+
+
+
+
+uploaded_file = st.file_uploader("Choose a file")
+option = st.radio(
+    "Do you want to show the negative impedance value",
+    ('Yes', 'No'),horizontal=True)
+circ_str = st.text_input('Circuit Elements (at least 2)', 'p(R,C)')
+
+# st.write('The current movie title is', type(circ_str))
+if len(circ_str) > 0:
+    names = circ_str.replace(' ', '').split('-')
+    fig, ax = plt.subplots()
+    
+    EEC=elements(ax,0,0,2)
+    EEC.long_line()
+    elem_arr =['R','C','W']
+    nx=0.5
+    ny=1
+    if len (names) ==1 and names[0] in elem_arr:
+        e = RuntimeError('This is an exception of type RuntimeError')
+        st.exception(e)
+        st.stop()
+    for elem in names:
+        if elem in elem_arr:
+            getattr(EEC, elem)()
+            EEC.line()
+        if 'p' == elem[0]:
+            elem_name = elem.replace('p', '').replace('(', '').replace(')', '').split(',')
+            print(elem_name)
+            elem_name_up = elem_name[0].split('*')
+            print(len(elem_name_up))
+            elem_name_low = elem_name[1].split('*')
+            elem_up = []
+            elem_low = []
+            for i in range (0,len(elem_name_up)):
+                if elem_name_up [i] in elem_arr:
+                    elem_up.append(getattr(EEC, elem_name_up[i]))
+                else:
+                    st.error('This is an error', icon="🚨")
+                    plt.clf()
+                    break
+                    
+            for i in range (0,len(elem_name_low)):
+                if elem_name_low [i] in elem_arr:
+                    elem_low.append(getattr(EEC, elem_name_low[i]))
+                else:
+                    st.error('This is an error', icon="🚨")
+                    plt.clf()
+                    break
+
+                
+            EEC.p(elem_up,elem_low)
+            EEC.line()
+            ny += 1
+    
+        nx += 0.5
+
+    EEC.line()
+    image_bytes = EEC.getimage(w=5*nx,h=1*ny,fmt='jpg')
+    st.image(image_bytes)
+else:
+    st.error('This is an error', icon="🚨")
+EEC_download = st.download_button(
+   label="Download image",
+   data=image_bytes,
+   file_name='Equivalent Circuit.png',
+   mime="image/png"
+)
+circ_str_0 = circ_str.replace('*', '-')
+elem_dic ={'R':0,'C':0,'W':0}
+p_num = {'R':1,'C':1,'W':2}
+circ_str_1 = '' 
+num_initial_guess = 0
+for i in range (len(circ_str)):
+    circ_str_1 += circ_str_0[i]
+    if circ_str_0[i] in elem_dic:
+        circ_str_1 += str(elem_dic[circ_str_0[i]])
+        elem_dic[circ_str_0[i]] +=1
+        num_initial_guess += p_num[circ_str_0[i]]
+        
+##
+default_guess = np.ones(num_initial_guess)*0.01
+default_guess = str(default_guess).replace(' ',',').replace('[','').replace(']','')
+initial_guess = st.text_input('Please input the initial guess or use the default value', default_guess)
+list1=list(initial_guess.split(','))
+initial_guess_1 = list(map(float,list1))
+##
+# agree = st.checkbox('Fit')
+circuit_1 = CustomCircuit(circ_str_1,initial_guess=initial_guess_1)
+
+if uploaded_file is not None:
+    fig, ax = plt.subplots()
+    f, Z = impedance_data_processing(uploaded_file,option)
+    plot_nyquist(ax, Z,label='data')
+    if st.button('Fit'):
+    # if agree:
+        circuit_1.fit(f,Z)
+        Z_fit = circuit_1.predict(f)
+        plot_nyquist(ax, Z_fit,label = 'fit')
+    plt.legend()
+    plt.tight_layout()
+        
+    st.pyplot(fig)
+fn = 'impedance.png'
+plt.savefig(fn)
+with open(fn, "rb") as img:
+    impedance_download = st.download_button(
+        label="Download image",
+        data=img,
+        file_name=fn,
+        mime="image/png"
+    )
+code = '''import numpy as np 
+import pandas as pd
+import scipy
+import matplotlib.pyplot as plt
+from impedance.models.circuits import CustomCircuit
+from impedance.visualization import plot_nyquist
+circ_str_1 = ''' +circ_str_1 +'''
+initial_guess_1 = ''' + str(list1)+'''
+circuit_1 = CustomCircuit(circ_str_1,initial_guess=initial_guess_1)
+circuit_1.fit(f,Z)
+Z_fit = circuit_1.predict(f)
+fig, ax = plt.subplots()
+plot_nyquist(ax, Z,label='data')
+plot_nyquist(ax, Z_fit,label = 'fit')
+plt.legend()
+plt.show()
+ '''
+st.code(code, language='python')
